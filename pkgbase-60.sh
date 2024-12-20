e670c59cf93b2ef54dbd69a7850e093cd33cd366
Sascha Greuel <github@1-2.dev>
2024-12-20 05:59:56 +0100

    Duplicate workflow logic to work with different repo visibility

--- pkgbase-60.sh
+++ pkgbase-60.sh
@@ -1,11 +1,13 @@
 #!/bin/bash 
 
 rm -rf pkgbase
-git clone https://github.com/SoftCreatRMedia/pkgbase-60-self-hosted.git pkgbase
+git clone https://github.com/SoftCreatRMedia/pkgbase-60.git pkgbase
 rm -rf pkgbase/.git
 rm -rf .github
+rm -f .gitignore
 rm -f .eslintignore
 rm -f .eslintrc.js
+rm -f eslint.config.mjs
 rm -f .php-cs-fixer.dist.php
 rm -f .phpcs.xml
 rm -f .prettierrc
